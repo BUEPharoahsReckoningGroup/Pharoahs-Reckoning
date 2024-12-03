9ccf46d881336113d1db28738d9e4b8a8762c460
Mosalehh <mohamedsaleh_166@icloud.com>
2024-12-03 21:10:55 +0200

    tile mapping

---
+++ Pharoah's Reckoning/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpHeight;
+
+    public KeyCode Spacebar;
+    public KeyCode L;
+    public KeyCode R;
+
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    public bool grounded;
+    public KeyCode Return;
+    public Transform FirePoint;
+    public GameObject bullet;
+
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+        if(Input.GetKeyDown(Spacebar) && grounded)
+        {
+            
+            Jump();
+        }
+
+        if(Input.GetKey(L))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+            if(GetComponent<SpriteRenderer>()!=null)
+            {
+                GetComponent<SpriteRenderer>().flipX = true;
+            }
+
+            
+        }
+
+        if(Input.GetKey(R))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+       
+            if(GetComponent<SpriteRenderer>()!=null)
+            {
+                GetComponent<SpriteRenderer>().flipX = false;
+            }
+
+            
+
+        }
+        anim.SetFloat("Speed", Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x));
+        anim.SetBool("Grounded", grounded);
+
+        if(Input.GetKeyDown(Return))
+        {
+            Shoot();
+        }
+    }
+
+    void Jump()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+        
+    }
+
+     void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+    public void Shoot()
+    {
+        Instantiate(bullet,FirePoint.position,FirePoint.rotation);
+    }
+}
