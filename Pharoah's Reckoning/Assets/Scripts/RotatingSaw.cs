acbd74ca6bac36ad09a924df08127edba06dc3dc
Mosalehh <mohamedsaleh_166@icloud.com>
2024-12-10 04:23:01 +0200

    Planting Traps
    1. Spikes
    2. Fire Trap
    3. Sand Worms
    4. Arrow shooter Trap

---
+++ Pharoah's Reckoning/Assets/Scripts/RotatingSaw.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingSaw : EnemyController
+{
+    private PlayerController player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player=FindObjectOfType<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position=Vector3.MoveTowards(transform.position,player.transform.position,maxSpeed * Time.deltaTime);
+    }
+}
